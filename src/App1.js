e0f255dabfe823774c4317f5e7ad6e0ff3459abd
Glorentino <86209684+Glorentino@users.noreply.github.com>
2022-08-19 00:07:15 -0400

    Add files via upload

---
+++ src/App1.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
+import VideoCall from "./VideoCall";
+
+function App1() {
+  const [inCall, setInCall] = useState(false);
+
+  return (
+    <div className="App" style={{height: "100%"}}>
+      {inCall ? (
+      <VideoCall setInCall={setInCall} />
+      ) : ( 
+      <Button 
+      variant="contained" 
+      color="primary" 
+      onClick={() => setInCall(true)}
+      >
+        Join Call
+      </Button>
+      )}
+    </div>
+  );
+}
+
+export default App1;
